d036868d11121c00f95e17697c1e0b7738c84f8a
xiaoye <1035017465@qq.com>
2022-06-28 09:10:45 +0800

    000

---
+++ AiXiu.Model/District/District.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AiXiu.Model
+{
+    /// <summary>
+    /// 区划类
+    /// </summary>
+    public class District
+    {
+        /// <summary>
+        /// 省份列表
+        /// </summary>
+        public List<Province> Provinces { get; set; }
+    }
+}
